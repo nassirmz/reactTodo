bbc3385ce5534b1834fd337a1a4d3a439a31a9a4
nassir <nassirmz@gmail.com>
2016-07-03 05:37:40 -0700

    (feat) Set initial todo data

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,6 +1,20 @@
 var React = require('react');
 
 var TodoApp = React.createClass({
+  getInitialState: function () {
+    return {
+      todos: [
+        {
+          id: 1,
+          task: 'Workout',
+        },
+        {
+          id: 2,
+          task: 'Clean room'
+        }
+      ];
+    };
+  },
   render: function () {
     return (
       <div>
