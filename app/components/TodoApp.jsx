c93d1128ff71da63c8614345581965266cb7e6d0
nassir <nassirmz@gmail.com>
2016-07-03 19:10:03 -0700

    (feat) Add TodoSearch component

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
+var TodoSearch = require('TodoSearch');
 
 var TodoApp = React.createClass({
   getInitialState: function () {
@@ -14,16 +15,25 @@ var TodoApp = React.createClass({
           id: 2,
           task: 'Clean room'
         }
-      ]
+      ],
+      showCompleted: false,
+      searchText: '',
     };
   },
   handleAddTodo: function (text) {
     alert('new todo: ' + text);
   },
+  handleSearch: function (showCompleted, searchText) {
+    this.setState({
+      showCompleted: showCompleted,
+      searchText: searchText.toLowerCase()
+    })
+  },
   render: function () {
     var {todos} = this.state;
     return (
       <div>
+        <TodoSearch onSearch={this.handleSearch} />
         <TodoList todos={todos}/>
         <AddTodo onAddTodo={this.handleAddTodo} />
       </div>
---
+++ app/components/TodoSearch.jsx
@@ -0,0 +1,29 @@
+var React = require('react');
+
+var TodoSearch = React.createClass({
+  handleSearch: function () {
+    var showCompleted = this.refs.showCompleted.checked;
+    //checked build in attribute of html5
+    var searchText = this.refs.searchText.value;
+    this.props.onSearch(showCompleted, searchText);
+  },
+  render: function () {
+    return (
+      <div>
+        <div>
+          <input type="search" ref="searchText" placeholder="Search Todo" onChange={this.handleSearch}/>
+        </div>
+        <div>
+          <label>
+            <input type="checkbox" ref="showCompleted" onChange={this.handleSearch} />
+            Show completed todos
+          </label>
+        </div>
+      </div>
+    )
+  }
+
+});
+
+module.exports = TodoSearch;
+//onChange attribute kind of like onclick or onSubmit but it gets called everytime the value is changing someone types a letter a, its gonna update rendering only todos that has A.
