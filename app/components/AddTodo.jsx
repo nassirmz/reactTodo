873a8a1c2bb022724cac633167f4873cfacec03e
nassir <nassirmz@gmail.com>
2016-07-04 18:41:49 -0700

    (enhancement) Center container div

--- app/components/AddTodo.jsx
+++ app/components/AddTodo.jsx
@@ -15,7 +15,7 @@ var AddTodo = React.createClass({
   },
   render: function () {
     return (
-      <div>
+      <div classname="container__footer">
         <form onSubmit={this.handleSubmit}>
           <input type="text" ref="todoText" />
           <button className="button extended">Add Todo</button>
