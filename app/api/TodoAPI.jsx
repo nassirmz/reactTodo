74914cbdaf01e58c6848824ec8267f50fe30a73c
nassir <nassirmz@gmail.com>
2016-07-04 17:35:44 -0700

    (feat) Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -17,7 +17,30 @@ module.exports = {
     } catch (e) {
 
     }
-    console.log(todos);
+
     return $.isArray(todos) ? todos : [];
+  },
+  filterTodos: function (todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+    filteredTodos = filteredTodos.filter((todo) => {
+      var todoText = todo.task.toLowerCase();
+      searchText = searchText.toLowerCase();
+      return searchText.length === 0 || todoText.indexOf(searchText) > -1;
+    });
+    filteredTodos.sort((a, b) => {
+      if(!a.completed && b.completed) {
+        return -1;
+      } else if (a.completed && !b.completed) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    return filteredTodos;
   }
 };
