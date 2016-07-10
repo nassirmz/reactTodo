363f6554aa5004a70d1338c0a8c0b0d354f7a382
nassir <nassirmz@gmail.com>
2016-07-10 01:02:48 -0700

    (refactor) Refactor middlewares to a separate file

--- app/app.jsx
+++ app/app.jsx
@@ -1,14 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Provider} = require('react-redux');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var {hashHistory} = require('react-router');
 
 var actions = require('actions');
 var store = require('configureStore').configure();
-var TodoAPI = require('TodoAPI');
-import Login from 'Login';
-import TodoApp from 'TodoApp';
 import firebase from 'app/firebase/';
+import router from 'app/router/';
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
@@ -26,29 +24,10 @@ $(document).foundation();
 // App css
 require('style!css!sass!applicationStyles');
 
-//middleware
-var requireLogin = (nextState, replace, next) => {
-  if (!firebase.auth().currentUser) {
-    replace('/');
-  }
-  next();
-};
-
-var redirectIfLoggedIn= (nextState, replace, next) => {
-  if(firebase.auth().currentUser) {
-    replace('/todos');
-  }
-  next();
-}
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={hashHistory}>
-      <Route path="/">
-        <Route path="todos" component={TodoApp} onEnter={requireLogin}/>
-        <IndexRoute component={Login} onEnter={redirectIfLoggedIn}/>
-      </Route>
-    </Router>
+    {router}
   </Provider>,
   document.getElementById('app')
 );
