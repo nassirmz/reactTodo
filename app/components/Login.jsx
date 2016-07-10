dfb1bcb3490e8c560a0c65049926345bc796aa99
nassir <nassirmz@gmail.com>
2016-07-09 21:16:20 -0700

    (feat) Add Login component and style it

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 export var Login = React.createClass({
   render() {
     return (
-    <p>Login component rendering</p>
+    <div>
+      <h1 className="page-title">Todo App</h1>
+      <div className="row">
+        <div className="columns small-centered small-10 medium-6 large-4">
+          <div className="callout callout-auth">
+            <h3>Login</h3>
+            <p>Login with Github account below</p>
+            <button className="button">Login With Github</button>
+          </div>
+        </div>
+      </div>
+    </div>
     );
 
   }
